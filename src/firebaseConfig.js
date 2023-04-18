62189dcd0864b5064bdaa1e50d478475f064c297
GokulVGAot <gokul.vg@aot-technologies.com>
2023-04-18 11:43:25 +0530

    added firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBPvnTY7doPmKPcJZ21RcaGVOG1vTECnHw",
+  authDomain: "linkedin-clone-43be6.firebaseapp.com",
+  projectId: "linkedin-clone-43be6",
+  storageBucket: "linkedin-clone-43be6.appspot.com",
+  messagingSenderId: "335987234808",
+  appId: "1:335987234808:web:7a8619042b7fb408f1680d",
+  measurementId: "G-9XPG3VCHQD"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
